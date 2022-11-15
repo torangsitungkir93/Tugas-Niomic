d6c93753083d3f998516b91003a0a148bbb253fb
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-15 13:19:56 +0700

    Menambahkan tugas 11

---
+++ Tugas Javascript/Tugas 11/README.md
@@ -0,0 +1,5 @@
+# Tugas 11 Javascript
+
+Var, Let, Const.
+
+![Tugas11](Screen/tugas11.png)
---
+++ Tugas Javascript/Tugas 11/Tugas11.js
@@ -0,0 +1,7 @@
+
+angka1= 2
+angka2= 4
+angka3= 5
+angka4= 10
+
+console.log(angka2*angka3+angka4/angka1)
