a113ed69e9ab70dad7adc02de64293fc98aa929f
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-15 12:48:20 +0700

    Menambahkan Tugas 8

---
+++ Tugas Javascript/Tugas 8/README.md
@@ -0,0 +1,5 @@
+# Tugas 8 Javascript
+
+Var, Let, Const.
+
+![Tugas8](Screen/tugas8.png)
---
+++ Tugas Javascript/Tugas 8/Tugas8.js
@@ -0,0 +1,14 @@
+
+
+const universitas = {
+    nama : "ITB",
+    akreditasi : "B",
+    alamat : "Bandung"
+};
+
+console.log(universitas.alamat);
+
+universitas.alamat = "Jakarta";
+
+console.log(universitas.alamat);
+
