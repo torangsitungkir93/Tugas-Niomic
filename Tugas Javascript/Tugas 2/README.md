910d57b8a947601f0f6f1182b7f463f610b18756
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-15 11:18:13 +0700

    first commit

---
+++ Tugas Javascript/Tugas 2/README.md
@@ -0,0 +1,5 @@
+# Tugas 2 Javascript
+
+Var, Let, Const.
+
+![Tugas2](screen/tugas2.png)
---
+++ Tugas Javascript/Tugas 2/Tugas2.js
@@ -0,0 +1,14 @@
+
+number = 2022;
+string = "November";
+boolean = true;
+tipeNull = null;
+var undefined;
+array = [1,7,3,8,6,5,6,2,3];
+
+
+console.log(number);
+console.log(string);
+console.log(boolean);
+console.log(tipeNull);
+console.log(array);
