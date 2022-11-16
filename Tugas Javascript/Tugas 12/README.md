644e9daab47f8475383ce8bda08e660df68dc32c
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-16 10:09:41 +0700

    Menambahkan Tugas 12

---
+++ Tugas Javascript/Tugas 12/README.md
@@ -0,0 +1,5 @@
+# Tugas 12 Javascript
+
+Var, Let, Const.
+
+![Tugas12](Screen/tugas12.png)
---
+++ Tugas Javascript/Tugas 12/Tugas12.js
@@ -0,0 +1,7 @@
+
+let buah = [`Pisang`,`Jeruk`,`Apel`,`Mangga`];
+
+buah.shift();
+buah.pop();
+
+console.log(buah);
