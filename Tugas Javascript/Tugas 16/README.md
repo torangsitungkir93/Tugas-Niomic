74973d1f1bc55e11907bfb0686f8c1ae0d9dc611
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-17 08:13:57 +0700

    Menambahkan Tugas 16

---
+++ Tugas Javascript/Tugas 16/README.md
@@ -0,0 +1,5 @@
+# Tugas 16 Javascript
+
+Menggunakan fungsu MIN dan MAX
+
+![Tugas16](Screen/tugas16.png)
---
+++ Tugas Javascript/Tugas 16/Tugas16.js
@@ -0,0 +1,9 @@
+
+const deret1 = [45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10];
+const deret2 = [45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5];
+
+console.log(`Deret 1   :${deret1}`);
+console.log(`Nilai MIN : ${Math.min(...deret1)}`);
+
+console.log(`Deret 2   :${deret2}`);
+console.log(`Nilai MAX : ${Math.max(...deret1)}`);
