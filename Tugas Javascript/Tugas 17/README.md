8e4087a6da617657ae23003f81052779eab1871f
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-17 09:51:44 +0700

    Menambahkan tugas 17

---
+++ Tugas Javascript/Tugas 17/README.md
@@ -0,0 +1,5 @@
+# Tugas 17 Javascript
+
+Menggunakan fungsi UpperCase dan LowerCase
+
+![Tugas17](Screen/tugas17.png)
---
+++ Tugas Javascript/Tugas 17/Tugas17.js
@@ -0,0 +1,18 @@
+
+
+console.log(`Tugas 1 `)
+const kalimat1 = `Saya beLajar bahaSa peMrograman deNgan khUsuk.`;
+const kalimat2 = `Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual.`;
+
+console.log(kalimat1.toLowerCase());
+console.log(kalimat2.toLowerCase());
+
+console.log(kalimat1.toUpperCase());
+console.log(kalimat2.toUpperCase());
+
+console.log(`-------------------`);
+console.log(`Tugas 2`);
+
+const index = "Lorem Ipsum is simply dummy text of the printing and typesetting industry";
+
+console.log(`${index.codePointAt(0)}-${index.codePointAt(1)}-${index.codePointAt(2)}-${index.codePointAt(3)}-${index.codePointAt(4)}-${index.codePointAt(5)}-${index.codePointAt(6)}`);
