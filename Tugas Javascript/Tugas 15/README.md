9032f3277e45a656f8ce2bb45bb73135ce2207e6
torangsitungkir93 <torangsitungkir93@gmail.com>
2022-11-17 08:04:44 +0700

    Menambahkan tugas 15

---
+++ Tugas Javascript/Tugas 15/README.md
@@ -0,0 +1,5 @@
+# Tugas 15 Javascript
+
+Menggunakan fungsu POW dan SQRT
+
+![Tugas15](Screen/tugas15.png)
---
+++ Tugas Javascript/Tugas 15/Tugas15.js
@@ -0,0 +1,9 @@
+
+let sisiTegak=6;
+let sisiSamping=8;
+let sisiMiring;
+
+sisiMiring = Math.sqrt(Math.pow(sisiTegak,2)+Math.pow(sisiSamping,2));
+
+console.log(Math.pow(sisiTegak,2));
+console.log(`Sisi Miring : ${sisiMiring}`);
